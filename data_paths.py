ab2b0367105d6b56032e2b6d7544e2c63d339500
Delio Jaramillo <64032852+Delio090418@users.noreply.github.com>
2025-10-02 23:36:11 +0200

    Update data_paths.py

--- data_paths.py
+++ data_paths.py
@@ -1,6 +1,6 @@
 data_paths={
         f"mnist":'path',
-        f"cifar10":'/Users/delio/Documents/Working_projects/Balazs/code_ppce/data',
+        f"cifar10":'path',
         f"BRAIN":'path',
         #f"pcam":["split_train_x","split_train_y"],
         #f"isic":["Training_Input","Training_GroundTruth"],
